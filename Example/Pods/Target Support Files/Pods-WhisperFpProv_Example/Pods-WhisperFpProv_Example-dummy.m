5b85223e33181eeefb8b00d6be1d53f860e4256e
ping xu <pingxu@pingdeMacBook-Air.local>
2022-08-08 17:53:02 +0800

    Initial WhisperFpProv

---
+++ Example/Pods/Target Support Files/Pods-WhisperFpProv_Example/Pods-WhisperFpProv_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WhisperFpProv_Example : NSObject
+@end
+@implementation PodsDummy_Pods_WhisperFpProv_Example
+@end
---
+++ Example/Pods/Target Support Files/WhisperFpProv/WhisperFpProv-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WhisperFpProv : NSObject
+@end
+@implementation PodsDummy_WhisperFpProv
+@end
